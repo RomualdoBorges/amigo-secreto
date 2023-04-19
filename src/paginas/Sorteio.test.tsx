4526ffef0aa9afc8f4ebc1e37cf8a992ff0e9d30
geomonitora@gmail.com <geomonitora@gmail.com>
2023-04-19 17:20:23 -0300

    Aula 5

--- src/paginas/Sorteio.test.tsx
+++ src/paginas/Sorteio.test.tsx
@@ -38,7 +38,7 @@ describe("na pagina de sorteio", () => {
       </RecoilRoot>
     );
     const opcoes = screen.queryAllByRole("option");
-    expect(opcoes).toHaveLength(participantes.length);
+    expect(opcoes).toHaveLength(participantes.length + 1); // porque já vem uma option por padrão
   });
 
   test("o amigo secreto é exibido quando solicitado", () => {
